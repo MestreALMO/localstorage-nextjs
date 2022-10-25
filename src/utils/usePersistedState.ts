16bae9489de4a7181b05f07529552cfbaf7a3170
André Lusegardis <lusegardis@gmail.com>
2022-10-25 08:47:34 -0300

    rocketseat help

--- src/utils/usePersistedState.ts
+++ src/utils/usePersistedState.ts
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 
 export const usePersistedState = (key: string, initialState: any) => {
   const [state, setState] = useState(() => {
-    useEffect(() => {
-      if (typeof window !== "undefined") {
+    if (typeof window !== "undefined") {
+      useEffect(() => {
         const storageValue = localStorage.getItem(key);
 
         if (storageValue) {
@@ -13,8 +13,8 @@ export const usePersistedState = (key: string, initialState: any) => {
         }
 
         localStorage.setItem(key, JSON.stringify(state));
-      }
-    }, []);
+      }, []);
+    }
   });
 
   return [state, setState];
